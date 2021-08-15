9df0909702e9f1a4fe8a94be71e802d54dbdb029
zhangpba <zhangpba@qq.com>
2021-08-16 00:50:46 +0800

    获取ip请求依赖

---
+++ study-common/src/main/java/com/study/util/IpUtils.java
@@ -0,0 +1,37 @@
+package com.study.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * ip帮助类
+ *
+ * @date 2021-08-15
+ */
+public class IpUtils {
+
+    /**
+     * 获取请求IP:
+     * 用户的真实IP不能使用request.getRemoteAddr()
+     * 这是因为可能会使用一些代理软件，这样ip获取就不准确了
+     * 此外我们如果使用了多级（LVS/Nginx）反向代理的话，ip需要从X-Forwarded-For中获得第一个非unknown的IP才是用户的有效ip。
+     */
+    public static String getRequestIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
+    }
+}
