554580eb0c151ba3200c617341adf27d43080c2a
answerzhang <answerzhang@tencent.com>
2022-04-25 14:42:08 +0800

    # 增加网关

--- study-zuul/src/main/java/com/study/zuul/StudyZuulApplication.java
+++ study-zuul/src/main/java/com/study/zuul/StudyZuulApplication.java
@@ -2,6 +2,7 @@ package com.study.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -11,8 +12,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * @EnableZuulProxy - 开启Zuul网关。
  * 当前应用是一个Zuul微服务网关。会在Eureka注册中心中注册当前服务。并发现其他的服务。
  * Zuul需要的必要依赖是spring-cloud-starter-zuul。
+ *
+ * exclude = DataSourceAutoConfiguration.class 是排除掉datasource的自动注解 2022-04-25
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableZuulProxy
