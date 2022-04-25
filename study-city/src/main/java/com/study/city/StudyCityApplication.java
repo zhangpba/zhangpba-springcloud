53b1596e56db6093c2c3321d6b76b878237d85ca
answerzhang <answerzhang@tencent.com>
2022-04-25 11:33:52 +0800

    # 增加网关

--- study-city/src/main/java/com/study/city/StudyCityApplication.java
+++ study-city/src/main/java/com/study/city/StudyCityApplication.java
@@ -3,13 +3,13 @@ package com.study.city;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zhangpba
  */
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableDiscoveryClient
 public class StudyCityApplication {
 
